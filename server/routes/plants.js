b6163b7b1aa36dba54b562874a02d3a14d0291c3
anirbas2 <sabrina.attos@gmail.com>
2025-07-25 15:20:42 +0200

    ajout des routes

---
+++ server/routes/plants.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const plantController = require('../controllers/plantController');
+const auth = require('../middlewares/auth');
+
+const router = express.Router();
+
+// Toutes les routes nécessitent une authentification
+router.use(auth);
+
+// Routes CRUD pour les plantes
+router.get('/', plantController.getUserPlants);
+router.get('/catalog/search', plantController.searchCatalog);
+router.get('/:id', plantController.getPlant);
+router.post('/', plantController.addPlant);
+router.put('/:id', plantController.updatePlant);
+router.delete('/:id', plantController.deletePlant);
+router.patch('/:id/stats', plantController.updatePlantStats);
+
+module.exports = router;
