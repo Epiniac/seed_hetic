cc1f281ca8e887348083623a5e7f854f148933bf
Epiniac <jacqueschen92@gmail.com>
2025-07-25 10:11:07 +0200

    Changing database location

--- server/config/config.js
+++ server/config/config.js
@@ -5,9 +5,5 @@ export const config = {
   mqtt: {
     brokerUrl: process.env.MQTT_BROKER_URL,
     topic: process.env.MQTT_TOPIC,
-  },
-  supabase: {
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_KEY,
-  },
+  }
 };
